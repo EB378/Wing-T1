b7537173ec7805e05f7b48a1fd53aafcd7f8c5cb
EAlexBen <107051871+EB378@users.noreply.github.com>
2025-01-30 19:09:38 +0200

    Update LogbookPage.tsx

--- components/members/logbook/LogbookPage.tsx
+++ components/members/logbook/LogbookPage.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
-import NewLog from "@/components/members/logbook/NewLog";
+import React from "react";
 import Logbook from "@/components/members/logbook/Logbook";
-import ToggleNew from "@/components/members/logbook/ToggleNew";
 import { useTranslations } from "next-intl";
 
 
@@ -13,32 +11,12 @@ interface LogProps {
 }
 
 const LogbookPage: React.FC<LogProps> = ({ currentUser }) => {
-  const [activeComponent, setActiveComponent] = useState<string>("logbook");
   const t = useTranslations("Logbook");
-
-  const renderComponent = () => {
-    switch (activeComponent) {
-      case "newLog":
-        return (
-          <>
-            <NewLog currentUser={{
-              UserId: currentUser.UserId
-            }} />
-          </>
-        );
-      default:
-        return;
-    }
-  };
 
   return (
     <div className="flex-1 w-screen flex flex-col gap-12">
       <h2 className="font-bold text-2xl m-4">{t("logbook")}</h2>
-      <ToggleNew activeComponent={activeComponent} setActiveComponent={setActiveComponent} />
-      <div className="flex flex-col gap-4">
-        {renderComponent()}
-      </div>
-      <Logbook currentUser={currentUser} logid={0}/>
+      <Logbook currentUser={currentUser} log_id={0}/>
     </div>
   );
 };
